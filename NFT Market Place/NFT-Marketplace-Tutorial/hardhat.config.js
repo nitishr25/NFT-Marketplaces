1563ecb990679e654cf52ffd4a8947847939980e
nitishr25 <166040143+nitishr25@users.noreply.github.com>
2024-11-22 10:15:12 +0530

    Add files via upload

---
+++ NFT Market Place/NFT-Marketplace-Tutorial/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
+    },
+    sepolia:{
+      url: "https://eth-sepolia.g.alchemy.com/v2/_MIKk02oqMjACmS5D6DHGMM7yyPQOIM3",
+      accounts: [ "a751a8d7941c68984899dbe078612e13365ed69f9496e1b2ef2bf83bb38a4a0a"]
+
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
